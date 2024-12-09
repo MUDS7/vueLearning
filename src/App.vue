cfc14030c61c66cf8168e7f6a206c1c3e7d1734b
许亲风 <1161352794@qq.com>
2024-12-09 23:43:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+
+<template>
+    <div class="app">
+      <h1>你好啊</h1>
+      <Person/>
+    </div>
+</template>
+
+<script lang="ts">
+  import Person from './components/person.vue';
+  export default {
+    name:'App',
+    components: { Person },
+  }
+</script>
+
+<style scoped>
+  .app{
+    background-color: #ddd;
+    box-shadow: 0 0 10px;
+    border-radius: 10px;
+    padding: 20px;
+  }
+</style>
